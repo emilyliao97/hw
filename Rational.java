d29533e2c1affe36902a5220e96485f953eb6dab
emilyliao97 <liao.emily022@gmail.com>
2013-11-18 17:08:54 -0500

    Create Rational.java

---
+++ Rational.java
@@ -0,0 +1,12 @@
+public class Rational {
+
+  //Phase numero uno
+
+  //Instance vars
+  private int numer;
+  private int denom;
+  
+  //Default constructor
+  public Rational() {
+    return 0/1;
+  }
